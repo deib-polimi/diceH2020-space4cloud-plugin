e67c477519c8b33b809b76e2aba82ace1c6cf181
giorgiopea <annatar93@gmail.com>
2017-01-25 12:42:57 +0100

    Using now apache httpclient

--- src/it/polimi/deib/dspace/net/NetworkManager.java
+++ src/it/polimi/deib/dspace/net/NetworkManager.java
@@ -1,12 +1,26 @@
 package it.polimi.deib.dspace.net;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -21,6 +35,7 @@ public class NetworkManager {
 	private static NetworkManager instance;
 	private static String rootEndpoint = "http://specclient1.dei.polimi.it:8080";
 	private static String alternativesEndpoint = rootEndpoint+"/alternatives";
+	private static String modelUploadEndpoint = rootEndpoint+"/files/upload";
 	
 	public static NetworkManager getInstance(){
 		if(instance != null){
@@ -39,32 +54,46 @@ public class NetworkManager {
 		connection.disconnect();
 	}
 	
-	public JSONObject fetchAlternatives() throws IOException, ParseException{
-		int responseCode;
-		BufferedReader in;
-		StringBuilder sb;
-		String line;
+	public JSONObject fetchAlternatives(){
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(alternativesEndpoint);
+		CloseableHttpResponse response;
+		String body;
 		JSONParser parser;
 		JSONObject json;
-		HttpURLConnection connection = this.openConnection(alternativesEndpoint);
-		connection.setRequestMethod("GET");
-		responseCode = connection.getResponseCode();
-		if(responseCode != 200){
-			System.err.println("Error: GET request for "+alternativesEndpoint+" has not been succesfull -> response code "+responseCode);
-			return null;
+		try {
+			response = httpclient.execute(httpget);
+			if(response.getStatusLine().getStatusCode() != 200){
+				return null;
+			}
+			else{
+				body = IOUtils.toString(response.getEntity().getContent(), "UTF-8");
+				parser = new JSONParser();
+				json = (JSONObject) parser.parse(body);
+				response.close();
+				return json;
+			}
+		} catch (UnsupportedOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		 in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		 sb = new StringBuilder();
-		 while((line = in.readLine()) != null){
-			 sb.append(line);
-		 }
-		 parser = new JSONParser();
-		 json = (JSONObject) parser.parse(sb.toString());
-		return json;
+		catch (ClientProtocolException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+			
 		
 	}
 	
-	public void sendModel(){
+	public void sendModel(File file) throws IOException{
 		
 	}
 	
