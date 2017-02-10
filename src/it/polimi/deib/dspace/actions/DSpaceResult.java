93981386812615f5d261fd6874be928d80f022f3
arlind1992 <arlind_rufi@hotmail.com>
2017-02-10 12:26:24 +0000

    Connected button with browser

--- src/it/polimi/deib/dspace/actions/DSpaceResult.java
+++ src/it/polimi/deib/dspace/actions/DSpaceResult.java
@@ -6,26 +6,17 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
 
-import it.polimi.deib.dspace.control.DICEWrap;
-import it.polimi.deib.dspace.ui.DSpaceWizard;
-import it.polimi.deib.dspace.ui.EmbeddedBrowser;
 import it.polimi.deib.dspace.ui.ResultPage;
 
 
 public class DSpaceResult implements IWorkbenchWindowActionDelegate{
-	private EmbeddedBrowser brows;
-	private IWorkbenchWindow window;
 	private ResultPage p;
 	@Override
 	public void run(IAction arg0) {
 		
-		brows=new EmbeddedBrowser("www.google.com");
-		brows.launch(
-				window.getWorkbench().getDisplay().getActiveShell());
-	//	p.displayUrl("www.google.com");
-		System.out.println("here");
+		p=new ResultPage(new Shell());
+		p.displayUrl("http://specclient1.dei.polimi.it:8018/resPub");
 		
 	
 	}
@@ -43,26 +34,6 @@ public class DSpaceResult implements IWorkbenchWindowActionDelegate{
 	}
 
 	@Override
-	public void init(IWorkbenchWindow arg0) {
-		this.window=arg0;
-		ResultPage p=new ResultPage("smsafpo");
-		
+	public void init(IWorkbenchWindow arg0) {		
 	}
-	public static Shell getShell() {
-		IWorkbenchWindow window = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow();
-		if (window == null) {
-			IWorkbenchWindow[] windows = PlatformUI.getWorkbench()
-					.getWorkbenchWindows();
-			if (windows.length > 0) {
-				return windows[0].getShell();
-			}
-		} else {
-			return window.getShell();
-		}
-		return null;
-	}
-	
-	
-
 }
--- src/it/polimi/deib/dspace/ui/ResultPage.java
+++ src/it/polimi/deib/dspace/ui/ResultPage.java
@@ -1,36 +1,24 @@
 package it.polimi.deib.dspace.ui;
 
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Shell;
 
-public class ResultPage extends WizardPage{
-
+public class ResultPage extends Dialog{
 	
 	private EmbeddedBrowser browser;
 	private Composite container;
+	private Shell shell;
 
-
-	public ResultPage(String name) {
+	public ResultPage(Shell name) {
 		super(name);
-		this.setTitle(name);
-	
-	}
-	
-	@Override
-	public void createControl(Composite arg0) {
-		container=new Composite(arg0, SWT.NONE);
-		setPageComplete(true);
-		getWizard().getContainer().updateButtons();
-		setControl(container);
+		shell=name;
 	}
 	
 	public void displayUrl(String URL){
 		browser=new EmbeddedBrowser(URL);
-		browser.launch(container);
-		getShell().setSize(800, 800);
-		
+		browser.launch(shell);
+		shell.setSize(800, 800);
+		shell.open();	
 	}
-	
-
 }
