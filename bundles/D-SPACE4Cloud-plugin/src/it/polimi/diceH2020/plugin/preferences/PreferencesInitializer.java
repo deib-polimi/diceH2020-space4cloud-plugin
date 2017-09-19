f508aeeaea594c930e77e46040e467ba32881e63
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2017-09-18 21:10:36 -0400

    Fix menu label and default working directory

--- bundles/D-SPACE4Cloud-plugin/src/it/polimi/diceH2020/plugin/preferences/PreferencesInitializer.java
+++ bundles/D-SPACE4Cloud-plugin/src/it/polimi/diceH2020/plugin/preferences/PreferencesInitializer.java
@@ -15,9 +15,7 @@ limitations under the License.
  */
 package it.polimi.diceH2020.plugin.preferences;
 
-import java.io.File;
-import java.nio.file.Paths;
-
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
@@ -25,17 +23,16 @@ import it.polimi.diceH2020.plugin.Activator;
 
 public class PreferencesInitializer extends AbstractPreferenceInitializer {
 
-    public PreferencesInitializer() {
-    }
+	public PreferencesInitializer() {}
 
-    @Override
-    public void initializeDefaultPreferences() {
-        IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-        store.setDefault(Preferences.FRONT_END_ADDRESS, "localhost");
-        store.setDefault(Preferences.FRONT_END_PORT, 8000);
-        store.setDefault(Preferences.BACK_END_ADDRESS, "localhost");
-        store.setDefault(Preferences.BACK_END_PORT, 8080);
-        store.setDefault(Preferences.SAVING_DIR, Paths.get(".").toAbsolutePath().normalize().toString() + File.separator);
-        store.setDefault(Preferences.SIMULATOR, Preferences.GSPN);
-    }
+	@Override
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault(Preferences.FRONT_END_ADDRESS, "localhost");
+		store.setDefault(Preferences.FRONT_END_PORT, 8000);
+		store.setDefault(Preferences.BACK_END_ADDRESS, "localhost");
+		store.setDefault(Preferences.BACK_END_PORT, 8080);
+		store.setDefault(Preferences.SAVING_DIR, ResourcesPlugin.getWorkspace().getRoot().getLocation().toString());
+		store.setDefault(Preferences.SIMULATOR, Preferences.GSPN);
+	}
 }
