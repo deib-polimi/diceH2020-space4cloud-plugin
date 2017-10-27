1ba0298ae22de8e81f8ccc0318193abe0f55c0c5
user <nraspa@live.it>
2017-10-27 17:41:16 +0200

    Added new option in preference page for DagSim Log Solver

--- bundles/D-SPACE4Cloud-plugin/src/it/polimi/diceH2020/plugin/preferences/Preferences.java
+++ bundles/D-SPACE4Cloud-plugin/src/it/polimi/diceH2020/plugin/preferences/Preferences.java
@@ -23,9 +23,12 @@ public class Preferences {
 	protected static final String FRONT_END_PORT = "FrontEndPort";
 	protected static final String BACK_END_ADDRESS = "BackEndAddress";
 	protected static final String BACK_END_PORT = "BackEndPort";
+	protected static final String DAG_SOLVER_ADDRESS = "DagSolverAddress";
+	protected static final String DAG_SOLVER_PORT = "DagSolverPort";
 	protected static final String JMT_PARSER = "JmtParser";
 	protected static final String SIMULATOR = "Simulator";
-
+	
+	
 	public static final String DAG_SIM = "dagSim";
 	public static final String GSPN = "GSPN";
 	public static final String JMT = "JMT";
@@ -38,6 +41,14 @@ public class Preferences {
 		return Activator.getDefault().getPreferenceStore().getInt(FRONT_END_PORT);
 	}
 
+	public static String getDagSolverAddress() {
+		return Activator.getDefault().getPreferenceStore().getString(DAG_SOLVER_ADDRESS);
+	}
+	
+	public static int getDagSolverPort() {
+		return Activator.getDefault().getPreferenceStore().getInt(DAG_SOLVER_PORT);
+	}
+	
 	public static String getBackEndAddress() {
 		return Activator.getDefault().getPreferenceStore().getString(BACK_END_ADDRESS);
 	}
@@ -52,6 +63,10 @@ public class Preferences {
 
 	public static String getBackEndUrl() {
 		return "http://" + getBackEndAddress() + ":" + getBackEndPort() + "/";
+	}
+	
+	public static String getDagSolverUrl() {
+		return "http://" + getDagSolverAddress() + ":" + getDagSolverPort() + "/";
 	}
 
 	public static String getSavingDir() {
